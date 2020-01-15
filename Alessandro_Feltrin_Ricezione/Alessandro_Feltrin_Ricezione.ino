0a137ceaa521a13e0f9106805cbce902a3f07019
Unknown <alessandro.feltrin@barsanti.edu.it>
2020-01-15 11:35:49 +0100

    15/01/2020

---
+++ Alessandro_Feltrin_Ricezione/Alessandro_Feltrin_Ricezione.ino
@@ -0,0 +1,11 @@
+String caratteri = "ABCDEFGHIJKLMNOPQRSTUVWXYZ."
+String[] codiceMorse = {"._","_...","_._.","_..",".",".._.","__.","....","..",".___","_._","._..","__","_.","___",".__.","__._","._.","...","_",".._","..._",".__","_.._","_.__","__..","._._._"}
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
